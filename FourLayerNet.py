e2adaf3a7940ec0149310d4a9379cd4c739afe93
nikolaimerritt <nikolaimerritt20@gmail.com>
2021-06-16 13:16:38 +0100

    Computes cost gradient using matrix multiplication in numpy -- tested against manual formulas

--- FourLayerNet.py
+++ FourLayerNet.py
@@ -1,16 +1,13 @@
 from typing import List
 import numpy as np
 
-def relu(x: float) -> float:
-    return 0 if x < 0 else x
+def relu(x):
+    return x * (x > 0)
 
 
-def drelu_dx(x: float) -> float:
-    return 0 if x < 0 else 1
+def drelu(x: float) -> float:
+    return 1.0 * (x > 0)
 
-
-def dataRelu(data: List[float]) -> List[float]:
-    return [relu(x) for x in data]
 
 
 def normalise(data: List[float]) -> List[float]:
@@ -37,11 +34,62 @@ class FourLayerNet:
     
 
     def calcOutputLayer(self, inputLayer):
-        layer1 = dataRelu(self.fstWeights @ inputLayer + self.fstBiases)
-        layer2 = dataRelu(self.sndWeights @ layer1 + self.sndBiases)
-        outputLayer = dataRelu(self.thdWeights @ layer2 + self.thdBiases)
+        layer1 = relu(self.fstWeights @ inputLayer + self.fstBiases)
+        layer2 = relu(self.sndWeights @ layer1 + self.sndBiases)
+        outputLayer = relu(self.thdWeights @ layer2 + self.thdBiases)
         return outputLayer
     
+
+    def numpyCostGradient(self, inputLayer, desiredOutputLayer):
+        """ computes the gradient of each weight and bias with one single output layer """
+        """ THE MATHS: 
+        Put: 
+            a as the 0th input layer
+            x, y, z as the 1st, 2nd, 3rd output layers before they have been relu'ed
+            F, S, T as the 1st, 2nd, 3rd weights
+            f, s, t as the 1st, 2nd, 3rd biases
+            w as the desired output layer
+        """
+        a = inputLayer
+        F, S, T = self.fstWeights, self.sndWeights, self.thdWeights
+        f, s, t = self.fstBiases, self.sndBiases, self.thdBiases
+        w = desiredOutputLayer
+        """
+        Then the cost is given by:
+            x_l = F_lm a_m + f_l                    (sum over m)
+            y_k = S_kl r(x_l) + s_k                 (sum over l)
+            z_j = T_jk r(y_k) + t_j                 (sum over k)
+
+            C = (r(z_j) - w_j)^2                    (sum over j)
+        """
+        x = F @ a + f
+        y = S @ relu(x) + s
+        z = T @ relu(y) + t
+        """
+        So the derivatives of C wrt x, y, z are:
+            dC/dz_j = 2 (r(z_j) - w_j) r'(z_j)
+            dC/dy_k =  T_jk dC/dz_j r'(y_k)          (sum over j)
+            dC/dx_l = dC/dy_k r'(x_l)               (sum over k)
+        """
+        dC_dz = 2 * (relu(z) - w) * drelu(z)
+        dC_dy = (np.transpose(T) @ dC_dz) * drelu(y)
+        dC_dx = drelu(x) * np.sum(dC_dy)
+        """
+        This means the derivatives wrt the parameters are:
+            dC/dt_j = dC/dz_j       dC/dT_jk = dC/dz_j r(y_k)
+            dC/ds_k = dC/dy_k       dC/dS_kl = dC/dy_k r(x_l)
+            dC/df_l = dC/dx_l       dC/dF_lm = dC/dx_l a_m
+        """
+        thdBiasGradient = dC_dz
+        thdWeightGradient = np.outer(dC_dz, relu(y))
+
+        sndBiasGradient = dC_dy
+        sndWeightGradient = np.outer(dC_dy, relu(x))
+
+        fstBiasGradient = dC_dx
+        fstWeightGradient = np.outer(dC_dx, a)
+    
+        return fstWeightGradient, fstBiasGradient, sndWeightGradient, sndBiasGradient, thdWeightGradient, thdBiasGradient
 
     def costGradient(self, inputLayer, desiredOutputLayer):
         """ computes the gradient of each weight and bias with one single output layer """
@@ -66,17 +114,17 @@ class FourLayerNet:
             C = (r(z_j) - w_j)^2                    (sum over j)
         """
         x = F @ a + f
-        y = S @ dataRelu(x) + s
-        z = T @ dataRelu(y) + t
+        y = S @ relu(x) + s
+        z = T @ relu(y) + t
         """
         So the derivatives of C wrt x, y, z are:
             dC/dz_j = 2 (r(z_j) - w_j) r'(z_j)
-            dC/dy_k = dC/dz_j T_jk r'(y_k)          (sum over j)
+            dC/dy_k =  T_jk dC/dz_j r'(y_k)          (sum over j)
             dC/dx_l = dC/dy_k r'(x_l)               (sum over k)
         """
-        dC_dz = lambda j:   2 * (relu(z[j]) - w[j]) * drelu_dx(z[j])
-        dC_dy = lambda k:   sum(dC_dz(j) * T[j][k] * drelu_dx(y[k]) for j in range(self.thdLayerSize))
-        dC_dx = lambda l:   sum(dC_dy(k) * drelu_dx(x[l]) for k in range(self.sndLayerSize))
+        dC_dz = lambda j:   2 * (relu(z[j]) - w[j]) * drelu(z[j])
+        dC_dy = lambda k:   sum(dC_dz(j) * T[j][k] * drelu(y[k]) for j in range(self.thdLayerSize))
+        dC_dx = lambda l:   sum(dC_dy(k) * drelu(x[l]) for k in range(self.sndLayerSize))
         """
         Which means the derivatives wrt the parameters are:
             dC/dt_j = dC/dz_j       dC/dT_jk = dC/dz_j r(y_k)
@@ -110,11 +158,10 @@ class FourLayerNet:
             [dC_dx(l) * a[m] for m in range(self.inputLayerSize)]
             for l in range(self.fstLayerSize)
         ])
-        fstWeightGradient.shape = (16, 784)
-    
+
         return fstWeightGradient, fstBiasGradient, sndWeightGradient, sndBiasGradient, thdWeightGradient, thdBiasGradient
 
-    
+
     def testDimensions(self, inputLayer, desiredOutputLayer):
         fstWeightGradient, fstBiasGradient, sndWeightGradient, sndBiasGradient, thdWeightGradient, thdBiasGradient = self.costGradient(inputLayer, desiredOutputLayer)
 
@@ -126,3 +173,17 @@ class FourLayerNet:
 
         thdWeightGradient - self.thdWeights
         thdBiasGradient - self.thdBiases
+    
+    
+    def testCostGradient(self, inputLayer, desiredOutputLayer):
+        mathsGrads = self.costGradient(inputLayer, desiredOutputLayer)
+        numpyGrads = self.numpyCostGradient(inputLayer, desiredOutputLayer)
+
+
+        for i in range(len(mathsGrads)):
+            error = mathsGrads[i] - numpyGrads[i]
+            avgError = np.sum(np.abs(error)) / error.size
+            if avgError > 0.1:
+                print("messed up with test", i + 1)
+                print("average error is", avgError)
+                break
--- main.py
+++ main.py
@@ -5,19 +5,8 @@ from FourLayerNet import FourLayerNet
 mndata = MNIST("samples")
 images, labels = mndata.load_training()
 image = np.array(images[0])
-print(image.shape)
+
 
 net = FourLayerNet(len(images[0]), 16, 16, 10)
-"""
-A = np.array([
-    [1, 2, 3], 
-    [4, 5, 6]
-])
-B = np.array([
-    [1 for m in range(3)]
-    for l in range(4)
-])
-print(B.shape)
-x = np.array([[10], [20], [30]])
-# print(net.calcOutputLayer(images[0]))
-"""
+desiredOutputLayer = np.array(list(range(10)))
+net.testCostGradient(images[0], desiredOutputLayer)
