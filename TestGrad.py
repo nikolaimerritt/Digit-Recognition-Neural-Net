cb3ad41047eea66d96b6a5ae27b8d6019f9ded3c
nikolaimerritt <nikolaimerritt20@gmail.com>
2021-06-18 15:35:30 +0100

    TestGrad has testAllWeights. all weights passed

--- TestGrad.py
+++ TestGrad.py
@@ -17,6 +17,8 @@ desOutLayers = [outLayerFromLabel(label) for label in labels]
 inLayer = images[0]
 desOutLayer = desOutLayers[0]
 params = FourLayerNet.getRandomParams(len(images[0]), 16, 16, 10)
+gradToTest = FourLayerNet.paramsGrad(inLayer, desOutLayer, params)
+epsilon = 10 ** (-5)
 
 
 def basisBias(biasNum, biasIdx):
@@ -25,21 +27,19 @@ def basisBias(biasNum, biasIdx):
     return basis
 
 
-def basisWeight(weightNum, weightIdx, weightCol):
+def basisWeight(weightNum, row, col):
     basis = 0 * params
-    basis.weights[weightNum][weightIdx][weightCol] = 1
+    basis.weights[weightNum][row][col] = 1
     return basis
 
 
 def numericBiasDeriv(biasNum, biasIdx):
     basisParam = basisBias(biasNum, biasIdx)
     change = lambda h: FourLayerNet.singleCost(inLayer, desOutLayer, params + h * basisParam)
-    epsilon = 10 ** (-5)
     return (change(epsilon) - change(-epsilon)) / (2 * epsilon)
 
 
-def testBias(biasNum, biasIdx):
-    gradToTest = FourLayerNet.paramsGrad(inLayer, desOutLayer, params)
+def relErrorInBias(biasNum, biasIdx):
     componentToTest = gradToTest.biases[biasNum][biasIdx]
     numericComponent = numericBiasDeriv(biasNum, biasIdx)
 
@@ -48,13 +48,40 @@ def testBias(biasNum, biasIdx):
     return abs(componentToTest - numericComponent) / numericComponent
 
 
+def numericWeightDeriv(weightNum, row, col):
+    basisParam = basisWeight(weightNum, row, col)
+    change = lambda h: FourLayerNet.singleCost(inLayer, desOutLayer, params + h * basisParam)
+    return (change(epsilon) - change(-epsilon)) / (2 * epsilon)
+    
+
+def relErrorInWeight(weightNum, row, col):
+    componentToTest = gradToTest.weights[weightNum][row][col]
+    numericComponent = numericWeightDeriv(weightNum, row, col)
+    if numericComponent == 0:
+        return abs(componentToTest - numericComponent)
+    else:
+        return abs(componentToTest - numericComponent) / numericComponent
+
+
 def testAllBiases():
     for biasNum in [2, 1, 0]:
         for biasIdx in range(len(params.biases[biasNum])):
-            relError = testBias(biasNum, biasIdx)
+            relError = relErrorInBias(biasNum, biasIdx)
             if relError > 10 ** (-5):
                 print("relative error", relError)
                 print("biasNum", biasNum)
                 print("biasIdx", biasIdx)
     
-testAllBiases()
+
+def testAllWeights():
+    for weightNum in [2, 1, 0]:
+        weight = params.weights[weightNum]
+        for row in range(len(weight)):
+            for col in range(len(weight[row])):
+                relError = relErrorInWeight(weightNum, row, col)
+                if relError > 10 ** (-4):
+                    print("relative error", relError)
+                    print("weightNum", weightNum)
+                    print("row", row)
+                    print("col", col)
+                    return
